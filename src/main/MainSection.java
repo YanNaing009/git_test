6164d8cb27827a45faaccce6a80b9e5e345f620f
YanNaingToe <yannaing.npttn@gmail.com>
2023-09-26 14:49:49 +0630

    updated add gitignore and change editor

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -3,6 +3,6 @@ package main;
 public class MainSection {
 	public static void main(String[] args) {
 		System.out.println("Inside main method.....");
-		System.out.println("Inside main method.....");
+		System.out.println("Copy Inside main method.....");
 	}
 }
