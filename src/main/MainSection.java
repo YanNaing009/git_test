971fc189f92dc201c0fc821907bd27cce8b9f8b8
YanNaingToe <yannaing.npttn@gmail.com>
2023-09-27 16:10:36 +0630

    added remote test

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -4,5 +4,6 @@ public class MainSection {
 	public static void main(String[] args) {
 		System.out.println("Inside main method.....");
 		System.out.println("Copy Inside main method.....");
+		System.out.println("Test to push remote master");
 	}
 }
